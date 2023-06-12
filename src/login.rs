f9f0f9ac15509916e92043666152735995858ef1
Buntin-Synthia <aokitakumi72@gmail.com>
2023-06-12 21:43:00 +0900

    user_idが形式にマッチしなければループする処理を消した。

--- src/login.rs
+++ src/login.rs
@@ -1,20 +1,17 @@
 use rpassword::prompt_password;
 use std::io;
+use std::io::Write;
 
 pub fn interactive_login() {
     let mut user_id = String::new();
-    loop {
-        println!("Please enter a user_id (minimum 4 characters):");
-        io::stdin()
-            .read_line(&mut user_id)
-            .expect("Failed to read line");
-        if user_id.trim().len() >= 4 {
-            break;
-        }
-        user_id.clear();
-        println!("User_id must have at least 4 characters. Please try again.");
-    }
+    print!("Please enter your user_id: ");
+    io::stdout().flush().expect("Failed to flush stdout");
+    io::stdin()
+        .read_line(&mut user_id)
+        .expect("Failed to read line");
+    let user_id = user_id.trim();
+
     let password = prompt_password("Please enter your password:").expect("Failed to read password");
-    println!("User_id: {}", user_id.trim());
+    println!("User_id: {}", user_id);
     println!("Password: {}", password);
 }
